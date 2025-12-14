2cd5815084674d78e61fe2bea681f2ff8be1ee14
viktoria naskova <smorodinka-gg@mail.ru>
2025-12-14 22:52:45 +0300

    style(WalletCard): add edit-wallet-icon (without logic), indentations, styles for text, spacing properties

--- src/components/WalletCard/WalletCard.jsx
+++ src/components/WalletCard/WalletCard.jsx
@@ -1,18 +1,26 @@
 import "./WalletCard.scss";
+import editWalletIcon from "../../assets/icons/edit-wallet.svg";
 
 const WalletCard = (props) => {
-  const {
-    name, 
-    amount,
-    walletColor,
-  } = props;
-  
+  const { name, amount, walletColor } = props;
+
   return (
     <div
       className="wallet-card"
       style={{ backgroundColor: walletColor || "#eee" }}
     >
-      <h2>{name}</h2>
+      <div className="wallet-card__title">
+        <h2>{name}</h2>
+        <img
+          className="wallet-card__icon"
+          src={editWalletIcon}
+          alt="Редактировать кошелек"
+          width={24}
+          height={24}
+          // onClick={}
+        />
+      </div>
+
       <span>{amount}</span>
     </div>
   );
